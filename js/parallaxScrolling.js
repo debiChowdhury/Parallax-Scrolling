c4bc2cad3c785d45ffc38a269db644fc99e92c46
unknown <debi.chowdhury@LMKOLPC146.learningmatekol.local>
2017-04-10 17:28:57 +0530

    second commit

--- js/parallaxScrolling.js
+++ js/parallaxScrolling.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function () {
-
+  // fadein dadeout
     $('.breadCrumbs').click(function(){
         $('.navbar').show(500);
         $('.close').show(500);
@@ -11,48 +11,41 @@ $(document).ready(function () {
       $('.navbar').hide(500);
       $('.close').hide(500);
         $('.breadCrumbs').show(500);
-
-
-    });
-
+  });
+// goto pages
 $('#menu li a').eq(0).click(function(){
 
   $('html, body').stop(true,false).animate({ scrollTop:0}, 1500);
-
 
 });
 $('#menu li a').eq(1).click(function(){
 
   $('html, body').stop(true,false).animate({ scrollTop:1431}, 1200);
 
-
 });
 $('#menu li a').eq(2).click(function(){
 
   $('html, body').stop(true,false).animate({ scrollTop:2231}, 1200);
-
 
 });
 $('#menu li a').eq(3).click(function(){
 
   $('html, body').stop(true,false).animate({ scrollTop:5331}, 1200);
 
-
 });
 $('#menu li a').eq(4).click(function(){
 
   $('html, body').stop(true,false).animate({ scrollTop:6865}, 1200);
-
 
 });
 $('#menu li a').eq(5).click(function(){
 
   $('html, body').stop(true,false).animate({ scrollTop:8354}, 1200);
 
-
 });
 
 });
+// background move
   var $cog = $('.wowSomeSauce'),
     $body = $(document.body),
    $bodyHeight = $('body').height();
@@ -74,51 +67,39 @@ $(window).on("scroll",function() {
     yPos = -( $(window).scrollTop() * 0.01);
     obj.css({'background-position':'0px '+yPos+'px'});
   });
-
-
+// animation
   if ($(window).scrollTop()> 0 && $(window).scrollTop()<150 ) {
-
-  $(".Bake").css({'bottom':  ($(window).scrollTop()*0.9) + 'px'});
+    $(".Bake").css({'bottom':  ($(window).scrollTop()*0.9) + 'px'});
   }
 
-     if ($(window).scrollTop()> 700 && $(window).scrollTop()<1400 ) {
+  if ($(window).scrollTop()> 700 && $(window).scrollTop()<1400 ) {
+     $(".wowSomeSauce").css({'top':  ($(window).scrollTop()*0.5) + 'px'});
+    }
 
-    $(".wowSomeSauce").css({'top':  ($(window).scrollTop()*0.5) + 'px'});
-  }
-
-if ($(window).scrollTop()> 2100 && $(window).scrollTop()<2900 ){
-
-$(".mushroom").css({'top':  ($(window).scrollTop()*0.236) + 'px'});
-$(".mushroomPizza").css({'top':  ($(window).scrollTop()*0.270) + 'px'});
+  if ($(window).scrollTop()> 2100 && $(window).scrollTop()<2900 ){
+    $(".mushroom").css({'top':  ($(window).scrollTop()*0.236) + 'px'});
+    $(".mushroomPizza").css({'top':  ($(window).scrollTop()*0.270) + 'px'});
   }
   if ($(window).scrollTop()> 2700 && $(window).scrollTop()<3431 ){
-
-  $(".garlic").css({'top':  ($(window).scrollTop()*0.149) + 'px'});
+    $(".garlic").css({'top':  ($(window).scrollTop()*0.149) + 'px'});
     $(".GarlicPizza").css({'top':  ($(window).scrollTop()*0.220) + 'px'});
-
     }
-    if ($(window).scrollTop()> 3600 && $(window).scrollTop()<4131 ){
 
+    if ($(window).scrollTop()> 3600 && $(window).scrollTop()<4131 ){
     $(".bacon").css({'top':  ($(window).scrollTop()*0.166) + 'px'});
     $(".baconPizza").css({'top':  ($(window).scrollTop()*0.191) + 'px'});
+    }
 
-      }
-      if ($(window).scrollTop()> 4200 && $(window).scrollTop()<5200 ){
-
+    if ($(window).scrollTop()> 4200 && $(window).scrollTop()<5200 ){
       $(".marshmellow").css({'top':  ($(window).scrollTop()*0.120) + 'px'});
-     $(".marshmellowPizza").css({'top':  ($(window).scrollTop()*0.150) + 'px'});
+      $(".marshmellowPizza").css({'top':  ($(window).scrollTop()*0.150) + 'px'});
+    }
 
-        }
+    if ($(window).scrollTop()> 6000 && $(window).scrollTop()<7300 ){
+      $(".about").css({'top':  ($(window).scrollTop()*0.030) + 'px'});
+    }
 
-        if ($(window).scrollTop()> 6000 && $(window).scrollTop()<7300 ){
-
-        $(".about").css({'top':  ($(window).scrollTop()*0.030) + 'px'});
-
-
-          }
-          if ($(window).scrollTop()> 6900 && $(window).scrollTop()<7800 ){
-
-
+    if ($(window).scrollTop()> 6900 && $(window).scrollTop()<7800 ){
          $(".layer1").css({'left':  (($(window).scrollTop()-$(".parallax11").offset().top)*0.5) + 'px'});
          $(".layer2").css({'top': (($(window).scrollTop()-$(".parallax11").offset().top)*0.7) + 'px'});
          $(".layer3").css({'top': (($(window).scrollTop()-$(".parallax11").offset().top)*0.9) + 'px'});
@@ -126,14 +107,11 @@ $(".mushroomPizza").css({'top':  ($(window).scrollTop()*0.270) + 'px'});
 
        }
 
-
-
-    if ($(this).scrollTop()> 415){
+   if ($(this).scrollTop()> 415){
       $(".tomatoSauce").css({"display":"block"})
                       .animate({"left":"2px","opacity":"1"},2200);
 
     }
-
     $('a').on('click', function(){
     $('a').removeClass('selected');
     $(this).addClass('selected');
